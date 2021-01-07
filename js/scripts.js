8b590ea921e0750c871f8d988394a99335280982
A H Zahin <azahinhasan@gmail.com>
2021-01-07 22:00:22 +0600

    a

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,20 @@
 var guessNumber = 0;
 var randomNumber=0;
 var i=0;
-var arr1 = [0,5,8,10];
-var arr2 = [0,2,1,9];
+var arr1 = [0,5,8,10,2];
+var arr2 = [0,2,1,9,4];
+var arr3 = [0,4,4,1,3];
 var points=0;
-var life = 10;
+var life = 08;
+var counterReset=0;
 var hintsMin = hintsMax = 0;
+
 window.onload = function() {
-   guessNumber=Math.floor(Math.random() * 1);
+   guessNumber=Math.floor(Math.random() * 2);
    randomNumber=Math.floor(Math.random() * 50);
    myFunction2();
-   
+   document.getElementById("buttonNext").disabled = true;
+   lifePoints();
 
  };
 
@@ -31,36 +35,60 @@ function hints1(){
 }
 function myFunction2() {  //next
    
-
+   document.getElementById("buttonClick").disabled = false;
+   document.getElementById("buttonNext").disabled = true;
    document.getElementById("unknownNumber").innerHTML = "?";
    if(guessNumber==0){
          randomNumber=randomNumber+arr1[i];
          i=i+1;
+         if(i>=4){
+            i = 0;
+         }
       
    }else if(guessNumber==1){
       randomNumber=randomNumber+arr2[i];
          i=i+1;
+         if(i>=4){
+            i = 0;
+         }
+   }else if(guessNumber==2){
+      randomNumber=randomNumber+arr3[i];
+         i=i+1;
+         if(i>=4){
+            i = 0;
+         }
    }
    hints1()
-   document.getElementById("point").innerHTML = randomNumber;
+   ///////////////////////////////
+   //document.getElementById("point").innerHTML = randomNumber;
    
  }
 
 
  function myFunction() {
+   document.getElementById("buttonNext").disabled = false;
+   document.getElementById("buttonClick").disabled = true;
    var yourNumber = document.getElementById("yourNumber").value;
    if(yourNumber == randomNumber){
+     
       document.getElementById("result").innerHTML = "Right";
       points=points+5;
+      
    }else{
       document.getElementById("result").innerHTML = "Wrong";
-      //document.getElementById("result").innerHTML = yourNumber;
+      
       life--;
    }
    document.getElementById("unknownNumber").innerHTML = randomNumber;
-   
+   lifePoints();
  }
 
+
+
+ function lifePoints(){
+   document.getElementById("life").innerHTML = ('0' + life).slice(-2);
+   document.getElementById("point").innerHTML = ('0' + points).slice(-2);
+ }
 
 
 
