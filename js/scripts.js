05df9dc74cdd40a7317bbf8aa7fb07d2dd825de3
A H Zahin <azahinhasan@gmail.com>
2021-01-08 10:31:36 +0600

    a

--- js/scripts.js
+++ js/scripts.js
@@ -30,14 +30,17 @@ function randomNumberMinMax(min, max){
 function hints1(){
    //document.getElementById("hints").innerHTML =randomNumber;
    hintsMin = randomNumberMinMax(randomNumber-2,randomNumber);
-   hintsMax = randomNumberMinMax(randomNumber,randomNumber+2);
-   hints = "[Hints:Number is bettween "+hintsMin+" and "+hintsMax+" ]";
+   hintsMax = randomNumberMinMax(randomNumber+1,randomNumber+3);
+   hints = "[Hints:Number is bettween "+hintsMin+" and "+hintsMax+"]";
 }
 function myFunction2() {  //next
    
    document.getElementById("buttonClick").disabled = false;
    document.getElementById("buttonNext").disabled = true;
    document.getElementById("unknownNumber").innerHTML = "?";
+   document.getElementById("hints").innerHTML = "";
+   hintsButtonShow();
+
    if(guessNumber==0){
          randomNumber=randomNumber+arr1[i];
          i=i+1;
@@ -105,107 +108,14 @@ function myFunction2() {  //next
 }
 
 function hintsButton(){
-   document.getElementById("hintsButton").style.display = "none";
+   //document.getElementById("hintsButton").style.display = "none";
+   document.getElementById("hintsButton").style.visibility  = "hidden";
    document.getElementById("hints").innerHTML = hints;
 }
 
-/*function myFunction() {
-    var size = document.getElementById("size").value;
-    var speed = document.getElementById("speed").value;
-    var sizeOption = document.getElementById("sizeOption").value;
-    var speedOption = document.getElementById("speedOption").value;
-    var fastCal=0;
-    var hours=00,days=00,min=00,sec=00;
-
-
-
-
-
-     if(sizeOption=='GB' && speedOption=="Mbps"){
-        fastCal=(size*1024)/(speed/8.5);
-        
-     }else if(sizeOption=='MB' && speedOption=="MBps"){
-        fastCal=(size/speed);
-     }else if(sizeOption=='MB' && speedOption=="Mbps"){
-        fastCal=size/(speed/8);
-     }else if(sizeOption=='GB' && speedOption=="MBps"){
-        fastCal=(size*1024)/(speed);
-     }
-
-
-
-     document.getElementById("days").innerHTML = fastCal;
-
-     fastCal=((fastCal/60)/60);
-
-   
-
-     if(fastCal>24){
-        thrdCal=fastCal/24;
-        days=(thrdCal + "").split(".")[0];
-        thrdCal = thrdCal.toFixed(8);
-        
-        fastCal=(thrdCal + "").split(".")[1];
-        
-        fastCal=fastCal*0.00000001;
-        
-        fastCal=(fastCal*24)-1;
-
-
-     }
-     fastCal = fastCal.toFixed(5);
-     hours=(fastCal + "").split(".")[0];
-    
-
-
-
-     secCal = (fastCal + "").split(".")[1];
-     secCal=secCal*0.00001;
-     secCal=(secCal*60)-1;
-
-     secCal = secCal.toFixed(3);
-      
-     min = (secCal + "").split(".")[0];
-     
-     thrCal = (secCal + "").split(".")[1];
-     thrCal=thrCal*0.001;
-     thrCal=thrCal*60;
-     thrCal = thrCal.toFixed(3);
-     sec = (thrCal + "").split(".")[0];
-     
-
-
-
-   if(days=="-0" || days==0 || days == 'NaN'){
-      days='00';
-   }
-   if(hours=="-0" || hours==0 || hours == 'NaN'){
-      hours='00';
-   }
-   if(min=="-0" || min==0 || min == 'NaN'){
-      min='00';
-   }
-   if(sec=="-0" || sec==0 || sec == 'NaN'){
-      sec='00';
-   }
-
-   document.getElementById("days").innerHTML = days;
-   document.getElementById("hours").innerHTML =  hours;
-   document.getElementById("min").innerHTML =  min;
-   document.getElementById("sec").innerHTML =  sec;
-
+function hintsButtonShow(){
+   //document.getElementById("hintsButton").style.display = "block text-align: center;";
+   document.getElementById("hintsButton").style.visibility = "visible";
+   document.getElementById("hints").innerHTML = "";
 }
 
-
-function copyEmail(value) {
-    var tempInput = document.createElement("input");
-    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
-    tempInput.value = value;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    alert("Copied my Email : " + tempInput.value);
-    document.body.removeChild(tempInput);
-
-   
-  }*/
